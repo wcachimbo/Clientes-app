e65a8b05669254c6836b89cbb522e552d746e0ed
wcachimbo <wcachimbo@smart-financial-systems.com>
2024-07-03 15:15:00 -0500

    add Valid of backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json'
 import { Cliente } from './cliente';
-import { Observable } from 'rxjs';
+import { Observable , throwError} from 'rxjs';
 import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -12,26 +15,63 @@ export class ClienteService {
 
   private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient , private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndPoint)
   }
 
-  create(cliente: Cliente): Observable<Cliente>{
-    return this.http.post<Cliente>(this.urlEndPoint,cliente, {headers: this.httpHeaders});
+  create(cliente: Cliente): Observable<any>{
+    return this.http.post<Cliente>(this.urlEndPoint,cliente, {headers: this.httpHeaders})
+    .pipe( 
+      catchError(e => {
+        if(e.status==400){
+          return throwError(e);
+        }
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.mensaje,"error");
+          return throwError(e);
+})
+    );
   }
 
   getCliente(id:number): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+     .pipe( 
+        catchError(e => {
+          this.router.navigate(['/clientes']);
+          console.error(e.error.mensaje);
+          Swal.fire(e.error.mensaje, e.error.mensaje,"error");
+            return throwError(e);
+  })
+
+    );
   }
 
-  update(cliente: Cliente): Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+  update(cliente: Cliente): Observable<any>{
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+      .pipe( 
+        catchError(e => {
+          if(e.status==400){
+            return throwError(e);
+          }  
+          console.error(e.error.mensaje);
+          Swal.fire(e.error.mensaje, e.error.mensaje,"error");
+            return throwError(e);
+  })
+      );
   }
 
   delete(id: number): Observable<Cliente>{
     return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+    .pipe( 
+      catchError(e => {
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.mensaje,"error");
+          return throwError(e);
+})
+    );
+
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -13,6 +13,8 @@ export class FormComponent implements OnInit{
   public cliente: Cliente= new Cliente();
   public titulo:string = "Crear Cliente";
 
+  public errores: string[] =[];
+
   constructor(private clienteService:ClienteService,
               private router:Router,
               private activatedRoute:ActivatedRoute){}
@@ -33,9 +35,14 @@ export class FormComponent implements OnInit{
   public create(): void{
     this.clienteService.create(this.cliente)
     .subscribe(
-      cliente => {
+      json => {
         this.router.navigate(['/clientes']);
-        swal.fire('Nuevo cliente',`Cliente ${cliente.nombre} creado con éxito!`,'success')
+        swal.fire('Nuevo cliente',`${json.mensaje}: ${json.cliente.nombre}`,'success')
+      },
+      err =>{
+        this.errores = err.error.errors as string[];
+        console.error('Codigo del error desde el backend: ' + err.status);
+        console.error(err.error.errors);
       }
     
   );}
@@ -43,9 +50,14 @@ export class FormComponent implements OnInit{
 
   update():void{
     this.clienteService.update(this.cliente)
-    .subscribe( cliente => {
+    .subscribe( json => {
       this.router.navigate(['/clientes'])
-      swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
+      swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre} `, 'success')
+    },
+    err =>{
+      this.errores = err.error.errors as string[];
+      console.error('Codigo del error desde el backend: ' + err.status);
+      console.error(err.error.errors);
     }
 
     )
